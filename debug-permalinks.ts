96c3a0a59f3986d8ebaa7952a083d3e138c331e5
sasisi344 <sasisi344@gmail.com>
2026-02-21 20:44:49 +0900

    clerar

--- debug-permalinks.ts
+++
@@ -1,5 +0,0 @@
-import { getPermalink } from './src/utils/permalinks';
-
-console.log('it-passport-quiz:', getPermalink('it-passport-quiz'));
-console.log('app/it-passport-quiz:', getPermalink('app/it-passport-quiz'));
-console.log('method (category):', getPermalink('method', 'category'));
